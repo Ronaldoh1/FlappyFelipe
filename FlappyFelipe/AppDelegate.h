49aa730f2ca5573f72007b67f9671b94a38e2bcc
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-25 18:17:43 -0700

    Initial Commit

---
+++ FlappyFelipe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlappyFelipe
+//
+//  Created by Ronald Hernandez on 10/25/15.
+//  Copyright © 2015 Wahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlappyFelipe/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  FlappyFelipe
+//
+
+//  Copyright (c) 2015 Wahoo. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
